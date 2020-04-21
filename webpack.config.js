8b1679bf197f0b7eb775c6fd363e7aab9286e04d
Malin Hanak <malinehanak@gmail.com>
2020-04-21 11:39:35 +0200

    added port

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devServer: {
     contentBase: './src',
     host: 'localhost',
-    port: 3000,
+    port: process.env.PORT || 3000,
     historyApiFallback: true,
     // overlay: {
     //   errors: true,
