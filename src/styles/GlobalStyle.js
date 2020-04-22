8a5988b9d3e805ac60e93155d42582895e3acd85
Malin Hanak <malinehanak@gmail.com>
2020-04-22 13:39:06 +0200

    rearranged css

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,45 +5,9 @@ import { cssReset } from './cssReset';
 export const GlobalStyle = createGlobalStyle`
   ${cssReset}
 
-  * { box-sizing: border-box; }
-
-  html {
-    font-size: 16px;
-    font-weight: 300;
-    background-color: #f4f3f3;
-  }
-  body {
-    width: 100vw;
-    max-width:100%;
-    height: 100%;
-    min-height: 110vh;
-    color: #385170;
-  }
-
   #root {
-    height: 100%;
-    margin-bottom: 0;
-    padding: 0;
-  }
-
-  .slide-in-left-enter {
-  transform: translateX(-100%);
-}
-
-.slide-in-left-enter-active {
-  transform: translateX(0);
-  opacity: 1;
-  transition: all 200ms;
-}
-
-.slide-in-left-exit {
-  transform: translateX(0%);
-  opacity: 1;
-}
-
-.slide-in-left-exit-active {
-  transform: translateX(-100%);
-  opacity: 0;
-  transition: all 200ms;
+  height: 100%;
+  margin-bottom: 0;
+  padding: 0;
 }
 `;
